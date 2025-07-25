3399d2abd6faf9ad2e6ae902c7a678a5d4887e71
gautam <gautamgupta.sg@gmail.com>
2025-07-25 16:10:54 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciFile
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 0, 1 };
+            int a = 0, b = 1, c;
+            for (int i = 2; i <= 15; i++)
+            {
+                c = a + b;
+                numbers.Add(c);
+                a = b;
+                b = c;
+            }
+
+            using (var writer = new System.IO.StreamWriter("fibonacci"))
+            {
+                try
+                {
+                    writer.WriteLine(string.Join(",", numbers));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred writing to the file: " + ex.Message);
+                }                
+            }
+        }
+    }
+}
